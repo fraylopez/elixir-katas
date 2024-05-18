1ee6ee2933ee486685196f7bfa4eaf14c50cc1cf
Victor Moreno <victor.moreno@cabify.com>
2024-05-18 13:21:14 +0200

    test: reorder

--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -6,15 +6,15 @@ defmodule HangmanTest do
     assert Hangman.guess("nadar", "a") == {:ok, "*a*a*"}
   end
 
+  test "should handle another good guess" do
+    assert Hangman.guess("nadar", "d") == {:ok, "**d**"}
+  end
+
   test "should handle a wrong guess" do
     assert Hangman.guess("nadar", "x") == {:bad_guess, "*****"}
   end
 
   test "should handle an invalid guess for a longer word" do
     assert Hangman.guess("ahogarse", "x") == {:bad_guess, "********"}
-  end
-
-  test "should handle another good guess" do
-    assert Hangman.guess("nadar", "d") == {:ok, "**d**"}
   end
 end
