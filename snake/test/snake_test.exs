2851493e2659576d1d3e15f50c92d82e644208bf
Victor Moreno <victor.moreno@cabify.com>
2024-05-18 20:48:07 +0200

    sdasd

--- snake/test/snake_test.exs
+++ snake/test/snake_test.exs
@@ -2,7 +2,7 @@ defmodule SnakeTest do
   use ExUnit.Case
   doctest Snake
 
-  test "move 4 directions" do
+  test "move with no input" do
     %{head: head} = Snake.move(%State{head: {1, 1}, looking_towards: :up})
     assert head == {1, 2}
     %{head: head} = Snake.move(%State{head: {1, 1}, looking_towards: :down})
@@ -13,11 +13,16 @@ defmodule SnakeTest do
     assert head == {0, 1}
   end
 
-  test "change looking towards" do
-    %{head: head, looking_towards: looking_towards} =
-      Snake.move(%State{head: {1, 1}, looking_towards: :up}, :right)
+  test "move towards look_towards" do
+    for test_case <- [{:up, {1, 2}}, {:right, {2, 1}}, {:down, {1, 0}}, {:left, {0, 1}}] do
+      look_towards = elem(test_case, 0)
+      expected_head = elem(test_case, 1)
 
-    assert head == {2, 1}
-    assert looking_towards == :right
+      %{head: head, looking_towards: looking_towards} =
+        Snake.move(%State{head: {1, 1}}, look_towards)
+
+      assert head == expected_head
+      assert looking_towards == look_towards
+    end
   end
 end
